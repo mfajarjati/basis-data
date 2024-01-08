682114cbd5a913d91e89d8073d1d119a13632961
mfajarjati <125721123+mfajarjati@users.noreply.github.com>
2024-01-08 22:33:13 +0700

    Update pemeriksa.py

--- halaman/pemeriksa.py
+++ halaman/pemeriksa.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pdfkit
+from streamlit_option_menu import option_menu
 from halaman.koneksi import create_connection
 
 connection, cursor = create_connection()
@@ -312,10 +313,21 @@ def delete():
 def main():
     st.title("Dashboard :violet[Tabel Pemeriksa] 👩🏻‍⚕️")
     st.header("", divider="rainbow")
-    page = st.sidebar.selectbox(
-        "Menu :",
-        ["DISPLAY", "INSERT", "UPDATE", "DELETE"],
-        key="pemeriksa_key",
+    page = option_menu(
+        menu_title=None,
+        options=["DISPLAY", "INSERT", "UPDATE", "DELETE"],
+        default_index=0,
+        orientation="horizontal",
+        icons=["eye", "pencil-square", "arrow-repeat", "trash"],
+        styles={
+            "icon": {"color": "white", "font-size": "12px"},
+            "nav-link": {
+                "font-size": "12px",
+                "text-align": "center",
+                "margin": "0px",
+                "--hover-color": "#666",
+            },
+        },
     )
 
     if page == "DISPLAY":
