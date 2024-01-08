8ac61fe7b2b002d1ff192cae52c17b2669a3bb5d
mfajarjati <125721123+mfajarjati@users.noreply.github.com>
2024-01-08 22:32:37 +0700

    Update ibu.py

--- halaman/ibu.py
+++ halaman/ibu.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 import pdfkit
+from streamlit_option_menu import option_menu
 from halaman.koneksi import create_connection
 
 connection, cursor = create_connection()
@@ -361,8 +362,21 @@ def delete():
 def main():
     st.title("Dashboard :green[Tabel Ibu] 👩🏻")
     st.header("", divider="rainbow")
-    page = st.sidebar.selectbox(
-        "Menu : ", ["DISPLAY", "INSERT", "UPDATE", "DELETE"], key="ibu_key"
+    page = option_menu(
+        menu_title=None,
+        options=["DISPLAY", "INSERT", "UPDATE", "DELETE"],
+        default_index=0,
+        orientation="horizontal",
+        icons=["eye", "pencil-square", "arrow-repeat", "trash"],
+        styles={
+            "icon": {"color": "white", "font-size": "12px"},
+            "nav-link": {
+                "font-size": "12px",
+                "text-align": "center",
+                "margin": "0px",
+                "--hover-color": "#666",
+            },
+        },
     )
 
     if page == "DISPLAY":
